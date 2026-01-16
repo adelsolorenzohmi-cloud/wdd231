12605918f441eb5d807626a2a5f440457bc34ddc
adelso.tzep <atzepchox@byupathway.edu>
2026-01-16 12:58:25 -0600

    Assignment completed

---
+++ scripts/prophets.js
@@ -0,0 +1,46 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProhetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+getProhetData()
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        //Create elemtns for birth info
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+
+        // Build the h2 content out to show the prophet's full name
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        // Add birth information
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        // Build the image portrait by setting all the relevant attributes
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name}lastname`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        // Append the section(card) with the created elements
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    }); // end of arrow function and forEach loop
+}
